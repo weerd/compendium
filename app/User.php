d05b349f8fbce0b4d70b57e311110c29dddd7963
Diego Lorenzo <diego@diegolorenzo.com>
2015-12-09 18:16:54 -0500

    Adding Laravel framework.

---
+++ app/User.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Foundation\Auth\User as BaseUser;
+
+class User extends BaseUser
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
